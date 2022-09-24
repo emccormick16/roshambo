4507529ccf4473a37d8294065df38aa2361b4bfd
Ed McCormick <edward.mccormick3@gmail.com>
2022-09-24 16:38:20 -0500

    setup

---
+++ db/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize("postgres://localhost:5444/sequelize_intro");
+
+const Games = db.define("game", {
+  result: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    isIn: [["computer", "human", "tie"]],
+  },
+});
+
+const Player = db.define("player", {
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+Player.hasMany(Games);
+Games.belongsTo(Player);
+
+module.exports = {
+  db,
+  Player,
+  Games,
+};
